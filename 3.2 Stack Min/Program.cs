a8355750d26e456557b4500e9709bfa7c2ef6180
Luca D'Angelo <info@lucadangelo.it>
2018-10-29 19:27:38 +0000

    3.2 done

--- 3.2 Stack Min/Program.cs
+++ 3.2 Stack Min/Program.cs
@@ -4,9 +4,11 @@ namespace _3._2_Stack_Min
 {
     class Program
     {
+        //LD Q3_02_StackMin
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Q3_02_StackMin.Run();
         }
+
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# CCI-stacks-and-queues
+# Stacks and Queues
+
+##3.2 Stack Min
+
 Implementation of CCI chapter 3, stack and queues
 9
