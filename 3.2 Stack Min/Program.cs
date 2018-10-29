331a21c77d63d407923e55ad2468b5fe7e625ef4
Luca D'Angelo <info@lucadangelo.it>
2018-10-29 19:22:19 +0000

    3.2

---
+++ 3.2 Stack Min/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3._2_Stack_Min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
